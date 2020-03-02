6201f605741a135cb4c86123a2c9354bdf1e5ab4
Bradley Graber <bradgraber@gmail.com>
2020-03-01 19:23:37 -0600

    Changed date format syntax

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -29,7 +29,7 @@ const Page: React.FC<any> = ({ match, setAppData, appData }) => {
           <IonButtons slot="start">
             <IonMenuButton />
           </IonButtons>
-          <IonTitle>{new Intl.DateTimeFormat('en-US', {weekday: "long", dateStyle: "full"}).format(new Date())}</IonTitle>
+          <IonTitle>{new Intl.DateTimeFormat('en-US', {weekday: "long", month: "long", year: "numeric", day: "numeric"}).format(new Date())}</IonTitle>
         </IonToolbar>
       </IonHeader>
 
